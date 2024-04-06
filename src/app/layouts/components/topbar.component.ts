e8e518b96ace38769027df279bb4347d20eb73c1
HamSPS123 <sengphachanh@verisette.com>
2024-04-07 00:36:45 +0700

    add logout

--- src/app/layouts/components/topbar.component.ts
+++ src/app/layouts/components/topbar.component.ts
@@ -2,11 +2,14 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  ElementRef,
   OnInit,
+  ViewChild,
 } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { CartService } from 'src/app/shared/services/cart.service';
 import { LayoutsService } from '../services/layouts.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-topbar',
@@ -14,26 +17,35 @@ import { LayoutsService } from '../services/layouts.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TopbarComponent implements OnInit {
+  @ViewChild('menu') menu!: ElementRef<HTMLDivElement>;
+  @ViewChild('btn') btn!: ElementRef<HTMLButtonElement>;
+  toggle: boolean = false;
   orderLength!: string;
   menuItems: MenuItem[] = [];
 
   constructor(
     public cartService: CartService,
     private cdr: ChangeDetectorRef,
-    public layoutService: LayoutsService
+    public layoutService: LayoutsService,
+    private authService: AuthService
   ) {
     // this.orderLength = this.cartService.getItemsLength();
     // this.cdr.markForCheck();
   }
 
   ngOnInit(): void {
-
-    // this.cartService.getItems.subscribe((values) => {
-    //   this.orderLength = values.length;
-    //   console.log(values);
-
-    //   this.cdr.markForCheck();
-    // });
+    document.addEventListener('click', (_e) => {
+      const menu = this.menu.nativeElement;
+      const btn = this.btn.nativeElement;
+      const target = _e.target as HTMLElement;
+      // if (!menu.contains(target) && !btn.contains(target)) {
+      if (!btn.contains(target)) {
+          menu.classList.remove('block');
+          menu.classList.add('hidden');
+          this.toggle = false
+      }
+      // menu.classList.toggle('hidden')
+  });
 
     this.menuItems = [
       {
@@ -44,5 +56,22 @@ export class TopbarComponent implements OnInit {
     ];
 
     this.orderLength = this.cartService.getItems().length.toString();
+  }
+
+  toggleMenu() {
+    const menu = this.menu.nativeElement;
+    if (this.toggle) {
+      menu.classList.add('hidden');
+      menu.classList.remove('block');
+      this.toggle = false;
+    } else {
+      menu.classList.add('block');
+      menu.classList.remove('hidden');
+      this.toggle = true;
+    }
+  }
+
+  logOut() {
+    this.authService.signOut();
   }
 }
