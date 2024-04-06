0bae79160027e30aee31761e20c96b8f45b1125a
HamSPS123 <sengphachanh@verisette.com>
2024-04-07 00:00:36 +0700

    docs

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   title: 'Production Environment Heading',
-  apiURL: 'http://103.114.146.8:3000'
+  apiURL: 'https://ban-cake.mangkonedev.tech/app'
 };
